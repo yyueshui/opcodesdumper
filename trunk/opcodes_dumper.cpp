77b0ec78d6884c3d6a23894bcb39da9573aec599
laruence@gmail.com <laruence@gmail.com@fae63790-895c-11dd-abeb-270e0dc42671>
2011-06-21 09:25:41 +0000

    提交修改

--- trunk/opcodes_dumper.cpp
+++ trunk/opcodes_dumper.cpp
@@ -1,13 +1,22 @@
 /*
-+----------------------------------------------------------+
- @FileName: opcodes_dumper.cpp
- @Touch Date: Fri 26 Sep 2008 10:16:08 AM CST
- @Author: 惠新宸(laruence@yahoo.com.cn)
- @Description: an PHP opcodes dumper base on embed sapi
-+----------------------------------------------------------+
-*/
-#include "sapi/embed/php_embed.h"
+  +----------------------------------------------------------------------+
+  | PHP Opcodes Dumper                                                   |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | http://www.php.net/license/3_01.txt                                  |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Author: Laruence  <laruence@gmail.com>                               |
+  +----------------------------------------------------------------------+
+   $Id$
+ */
+
 #include <cstdio>
+#include "sapi/embed/php_embed.h"
 #define BUFFER_LEN 40
 
 char *opname(zend_uchar opcode){
